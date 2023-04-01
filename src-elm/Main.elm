38adf58643992266a08e3870f88731a793ee52bc
Lucasmercado101 <lucasmercado101@gmail.com>
2023-04-01 20:34:34 -0300

    can move when holding down left mouse

--- src-elm/Main.elm
+++ src-elm/Main.elm
@@ -5,7 +5,7 @@ import Browser.Events exposing (onMouseMove)
 import Debug exposing (toString)
 import Html exposing (..)
 import Html.Attributes exposing (style)
-import Html.Events exposing (on, onClick)
+import Html.Events exposing (on, onClick, onMouseDown, onMouseUp)
 import Json.Decode as JD exposing (Decoder)
 import Svg as S exposing (rect, svg)
 import Svg.Attributes as SvgA exposing (cx, cy, fill, fontSize, height, r, rx, ry, textAnchor, version, viewBox, width, x, y)
@@ -16,8 +16,12 @@ import Svg.Attributes as SvgA exposing (cx, cy, fill, fontSize, height, r, rx, r
 
 
 subscriptions : Model -> Sub Msg
-subscriptions _ =
-    onMouseMove (mouseMoveDecoder |> JD.map MouseMove)
+subscriptions model =
+    if model.holdingLeftMouseDown then
+        onMouseMove (mouseMoveDecoder |> JD.map MouseMove)
+
+    else
+        Sub.none
 
 
 mouseMoveDecoder : Decoder ( Int, Int )
@@ -32,7 +36,9 @@ mouseMoveDecoder =
 
 
 type alias Model =
-    { view : ( Int, Int ) }
+    { view : ( Int, Int )
+    , holdingLeftMouseDown : Bool
+    }
 
 
 
@@ -42,6 +48,7 @@ type alias Model =
 init : () -> ( Model, Cmd Msg )
 init _ =
     ( { view = ( 0, 0 )
+      , holdingLeftMouseDown = False
       }
     , Cmd.none
     )
@@ -55,6 +62,8 @@ type Msg
     = NoOp
     | Clicked
     | MouseMove ( Int, Int )
+    | MouseDown
+    | MouseUp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -68,6 +77,12 @@ update msg model =
 
         MouseMove ( x, y ) ->
             ( { model | view = ( x, y ) }, Cmd.none )
+
+        MouseDown ->
+            ( { model | holdingLeftMouseDown = True }, Cmd.none )
+
+        MouseUp ->
+            ( { model | holdingLeftMouseDown = False }, Cmd.none )
 
 
 main : Program () Model Msg
@@ -87,7 +102,7 @@ view model =
             model.view
     in
     div
-        [ style "background-color" background, style "width" "100vw", style "height" "100vh" ]
+        [ style "background-color" background, style "width" "100vw", style "height" "100vh", onMouseDown MouseDown, onMouseUp MouseUp ]
         [ svg [ version "1.1", width "800", height "800", viewBox "0 0 800 800" ]
             [ rect [ width "50", height "50", fill "white", x (xPos |> toString), y (yPos |> toString) ] []
             ]
