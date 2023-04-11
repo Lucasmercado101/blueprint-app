86cdb1545f6c9f0f3f7191affff40b90efc344da
Lucasmercado101 <lucasmercado101@gmail.com>
2023-04-10 21:15:55 -0300

    added fns

--- src-elm/Point.elm
+++ src-elm/Point.elm
@@ -9,3 +9,18 @@ x ( x1, y1 ) =
 y : ( number, number ) -> number
 y ( x1, y1 ) =
     y1
+
+
+add : ( number, number ) -> ( number, number ) -> ( number, number )
+add ( x1, y1 ) ( x2, y2 ) =
+    ( x1 + x2, y1 + y2 )
+
+
+subtract : ( number, number ) -> ( number, number ) -> ( number, number )
+subtract ( x1, y1 ) ( x2, y2 ) =
+    ( x1 - x2, y1 - y2 )
+
+
+scale : number -> ( number, number ) -> ( number, number )
+scale factor ( x1, y1 ) =
+    ( x1 * factor, y1 * factor )
