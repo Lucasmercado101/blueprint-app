c101661afdbf4212ff090777eea30262f6fb3754
Lucasmercado101 <lucasmercado101@gmail.com>
2023-04-10 21:20:57 -0300

    added distance fn

--- src-elm/Point.elm
+++ src-elm/Point.elm
@@ -24,3 +24,16 @@ subtract ( x1, y1 ) ( x2, y2 ) =
 scale : number -> ( number, number ) -> ( number, number )
 scale factor ( x1, y1 ) =
     ( x1 * factor, y1 * factor )
+
+
+distance : ( Float, Float ) -> ( Float, Float ) -> Float
+distance ( x1, y1 ) ( x2, y2 ) =
+    -- # Math: \sqrt{(x_2 - x_1)^2 + (y_2 - y_1)^2}
+    let
+        dx =
+            x2 - x1
+
+        dy =
+            y2 - y1
+    in
+    sqrt (dx * dx + dy * dy)
