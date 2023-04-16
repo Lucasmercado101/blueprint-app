580d23bb3a21ba12c5df472e4687dd96cc60cf2a
Lucasmercado101 <lucasmercado101@gmail.com>
2023-04-15 22:25:46 -0300

    added TODO

--- src-elm/Rect.elm
+++ src-elm/Rect.elm
@@ -193,6 +193,7 @@ isThereAnyOverlap firstRectangle secondRectangle =
         || (x2 + width2 >= x1 && x2 + width2 <= x1 + width1 && y2 >= y1 && y2 <= y1 + height1)
         || (x2 >= x1 && x2 <= x1 + width1 && y2 + height2 >= y1 && y2 + height2 <= y1 + height1)
         || (x2 + width2 >= x1 && x2 + width2 <= x1 + width1 && y2 + height2 >= y1 && y2 + height2 <= y1 + height1)
+        -- TODO: this might be unnecesary, recheck math
         -- rectangle A is inside of rectangle B
         || (y2 >= y1 && y2 + height2 <= y1 + height1 && x2 <= x1 && x2 + width2 >= x1 + width1)
         -- rectangle B is inside of rectangle A
